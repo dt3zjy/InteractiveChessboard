9c985b7974492fe176eb6eac2a73fca583947c9d
Justin LaKier <grandforksuary@gmail.com>
2022-04-26 23:26:25 -0400

    Bishop, Knight, and Queen movement
    We could switch Knight to "h" for "horse" with King as "k" if we want to add it's movement, which is just omnidirectional Pawn movement or limited Queen movement.

--- chess.ino
+++ chess.ino
@@ -33,7 +33,7 @@ void checkPawn(struct Tile p) {
 }
 
 void checkRook(struct Tile p) {
-  Serial.print("Tile: ");
+  Serial.print("Rook, Tile: ");
       Serial.print("X: ");
       Serial.print(p.x);
       Serial.print(" Y: ");
@@ -48,7 +48,6 @@ void checkRook(struct Tile p) {
       break;
     }
   }
-
   for (int y = p.y; y >= 0; y--) {
     if (y == p.y) {
       continue;
@@ -59,7 +58,6 @@ void checkRook(struct Tile p) {
       break;
     }
   }
-
   for (int x = p.x; x < 3; x++) {
     if (x == p.x) {
       continue;
@@ -70,7 +68,6 @@ void checkRook(struct Tile p) {
       break;
     }
   }
-
   for (int x = p.x; x >= 0; x--) {
     if (x == p.x) {
       continue;
@@ -81,9 +78,219 @@ void checkRook(struct Tile p) {
       break;
     }
   }
-
   board[p.x][p.y].state = -1;
-  
+  printBoard(board);
+}
+
+void checkBishop(struct Tile p) {
+  Serial.print("Bishop, Tile: ");
+      Serial.print("X: ");
+      Serial.print(p.x);
+      Serial.print(" Y: ");
+      Serial.println(p.y);
+  int xmax = 2- p.x;
+  int ymax = 2- p.y;
+      //Serial.print("right max: ");
+      //Serial.print(2-p.x);
+      //Serial.print("down max: ");
+      //Serial.println(2- p.y);
+      //Serial.print("left max: ");
+      //Serial.print(p.x);
+      //Serial.print("up max: ");
+      //Serial.println(p.y);
+  for (int mod = 0; mod <= xmax; mod++) {
+    if (mod == 0) {
+       continue;
+    }
+    if (board[p.x+mod][p.y+mod].state == 0) {
+      //Serial.print("Bishop can move Down Right by");
+      //Serial.println(mod);
+      board[p.x+mod][p.y+mod].state = 2;
+    } else {
+      break;
+    }
+  }
+  for (int mod = 0; mod <= xmax && mod <= p.y; mod++) {
+    if (mod == 0) {
+       continue;
+    }
+    if (board[p.x+mod][p.y-mod].state == 0) {
+      //Serial.print("Bishop can move Up Right by");
+      //Serial.println(mod);
+      board[p.x+mod][p.y-mod].state = 2;
+    } else {
+      break;
+    }
+  }
+  for (int mod = 0; mod <= p.y && mod <= p.x; mod++) {
+    if (mod == 0) {
+       continue;
+    }
+    if (board[p.x-mod][p.y-mod].state == 0) {
+      //Serial.print("Bishop can move Up Left by");
+      //Serial.println(mod);
+      board[p.x-mod][p.y-mod].state = 2;
+    } else {
+      break;
+    }
+  }
+  for (int mod = 0; mod <= ymax && mod <= p.x; mod++) {
+    if (mod == 0) {
+      continue;
+    }
+    if (board[p.x-mod][p.y+mod].state == 0) {
+      //Serial.print("Bishop can move Down Left by");
+      //Serial.println(mod);
+      board[p.x-mod][p.y+mod].state = 2;
+    } else {
+      break;
+    }
+  }
+  board[p.x][p.y].state = -1;
+  printBoard(board);
+}
+
+void checkKnight(struct Tile p) {
+  Serial.print("Knight, Tile: ");
+      Serial.print("X: ");
+      Serial.print(p.x);
+      Serial.print(" Y: ");
+      Serial.println(p.y);
+  int xmax = 2- p.x;
+  int ymax = 2- p.y;
+  if (board[p.x+1][p.y+2].state == 0 && 1+p.x <= xmax && 2+p.y <= ymax) {
+    board[p.x+1][p.y+2].state = 2;
+  }
+  if (board[p.x+2][p.y+1].state == 0 && 2+p.x <= xmax && 1+p.y <= ymax) {
+    board[p.x+2][p.y+1].state = 2;
+  }
+  if (board[p.x-1][p.y-2].state == 0 && p.x-1 >= 0 && p.y-2 >= 0) {
+    board[p.x-1][p.y-2].state = 2;
+  }
+  if (board[p.x-2][p.y-1].state == 0 && p.x-2 >= 0 && p.y-1 >= 0) {
+    board[p.x-2][p.y-1].state = 2;
+  }
+  if (board[p.x+1][p.y-2].state == 0 && 1+p.x <= xmax && p.y-2 >= 0) {
+    board[p.x+1][p.y-2].state = 2;
+  }
+  if (board[p.x+2][p.y-1].state == 0 && 2+p.x <= xmax && p.y-1 >= 0) {
+    board[p.x+2][p.y-1].state = 2;
+  }
+  if (board[p.x-1][p.y+2].state == 0 && 2+p.y <= ymax && p.x-1 >= 0) {
+    board[p.x-1][p.y+2].state = 2;
+  }
+  if (board[p.x-2][p.y+1].state == 0 && 1+p.y <= ymax && p.x-2 >= 0) {
+    board[p.x-2][p.y+1].state = 2;
+  }
+  board[p.x][p.y].state = -1;
+  printBoard(board);
+}
+
+void checkQueen(struct Tile p) {
+  Serial.print("Queen, Tile: ");
+      Serial.print("X: ");
+      Serial.print(p.x);
+      Serial.print(" Y: ");
+      Serial.println(p.y);
+  int xmax = 2- p.x;
+  int ymax = 2- p.y;
+      //Serial.print("right max: ");
+      //Serial.print(2-p.x);
+      //Serial.print("down max: ");
+      //Serial.println(2- p.y);
+      //Serial.print("left max: ");
+      //Serial.print(p.x);
+      //Serial.print("up max: ");
+      //Serial.println(p.y);
+  for (int mod = 0; mod <= xmax; mod++) {
+    if (mod == 0) {
+       continue;
+    }
+    if (board[p.x+mod][p.y+mod].state == 0) {
+      //Serial.print("Bishop can move Down Right by");
+      //Serial.println(mod);
+      board[p.x+mod][p.y+mod].state = 2;
+    } else {
+      break;
+    }
+  }
+  for (int mod = 0; mod <= xmax && mod <= p.y; mod++) {
+    if (mod == 0) {
+       continue;
+    }
+    if (board[p.x+mod][p.y-mod].state == 0) {
+      //Serial.print("Bishop can move Up Right by");
+      //Serial.println(mod);
+      board[p.x+mod][p.y-mod].state = 2;
+    } else {
+      break;
+    }
+  }
+  for (int mod = 0; mod <= p.y && mod <= p.x; mod++) {
+    if (mod == 0) {
+       continue;
+    }
+    if (board[p.x-mod][p.y-mod].state == 0) {
+      //Serial.print("Bishop can move Up Left by");
+      //Serial.println(mod);
+      board[p.x-mod][p.y-mod].state = 2;
+    } else {
+      break;
+    }
+  }
+  for (int mod = 0; mod <= ymax && mod <= p.x; mod++) {
+    if (mod == 0) {
+      continue;
+    }
+    if (board[p.x-mod][p.y+mod].state == 0) {
+      //Serial.print("Bishop can move Down Left by");
+      //Serial.println(mod);
+      board[p.x-mod][p.y+mod].state = 2;
+    } else {
+      break;
+    }
+  }
+  for (int y = p.y; y < 3; y++) {
+    if (y == p.y) {
+      continue;
+    }
+    if (board[p.x][y].state == 0) {
+      board[p.x][y].state = 2;
+    } else {
+      break;
+    }
+  }
+  for (int y = p.y; y >= 0; y--) {
+    if (y == p.y) {
+      continue;
+    }
+    if (board[p.x][y].state == 0) {
+      board[p.x][y].state = 2;
+    } else {
+      break;
+    }
+  }
+  for (int x = p.x; x < 3; x++) {
+    if (x == p.x) {
+      continue;
+    }
+    if (board[x][p.y].state == 0) {
+      board[x][p.y].state = 2;
+    } else {
+      break;
+    }
+  }
+  for (int x = p.x; x >= 0; x--) {
+    if (x == p.x) {
+      continue;
+    }
+    if (board[x][p.y].state == 0) {
+      board[x][p.y].state = 2;
+    } else {
+      break;
+    }
+  }
+  board[p.x][p.y].state = -1;
   printBoard(board);
 }
 
@@ -97,6 +304,12 @@ struct Tile findPickedUpPiece(struct Tile prev[3][3], struct Tile b[3][3]) {
           checkPawn(t);
         } else if (t.piece == 'r') {
           checkRook(t);
+        } else if (t.piece == 'b') {
+          checkBishop(t);
+        } else if (t.piece == 'k') {
+          checkKnight(t);
+        } else if (t.piece == 'q') {
+          checkQueen(t);
         }
         return t;
       }
@@ -140,13 +353,12 @@ void setup() {
   }
 
   // Testing pawn in center
+  prevBoard[2][0].state = 1;
+  prevBoard[2][0].piece = 'k';
   prevBoard[1][0].state = 1;
   prevBoard[1][0].piece = 'p';
   board[1][0].state = 1;
   board[1][0].piece = 'p';
-  
-  prevBoard[2][0].state = 1;
-  prevBoard[2][0].piece = 'r';
 }
 
 void copyBoard() {
@@ -186,7 +398,8 @@ void loop() {
         }
       }
       board[1][0].state = 1;
-      board[2][2].state = 1;
+      // board[2][2].state = 1;
+      board[1][2].state = 1;
 
       Serial.print("New Previous Board: \n");
       printBoard(prevBoard);
@@ -194,7 +407,7 @@ void loop() {
       printBoard(board);
       Serial.println();
 
-      struct Tile p = findPlacedPiece('r', prevBoard, board);
+      struct Tile p = findPlacedPiece('k', prevBoard, board);
       
       f = true;
     }
